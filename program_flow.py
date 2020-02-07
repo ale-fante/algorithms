032c01937072a6a6a781fc26ce43d5b864a7e9f8
ale-fante <alejandra.hledesma@gmail.com>
2020-02-06 20:56:08 -0800

    turtle_pf_ex

---
+++ program_flow.py
@@ -0,0 +1,43 @@
+import turtle
+
+window = turtle.Screen()
+
+window.bgcolor("SlateBlue")
+window.title("Hello, Ale!")
+
+tess = turtle.Turtle()
+tess.color("hotpink")
+tess.pensize(5)
+
+alex = turtle.Turtle()
+alex.color("white")
+alex.pensize(2)
+
+jorge = turtle.Turtle()
+jorge.color("yellow")
+jorge.pensize(3)
+
+tess.forward(80)
+
+tess.left(120)
+tess.forward(80)
+tess.left(120)
+tess.forward(80)
+tess.left(120) # Complete the triangle
+
+# Draw square
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+
+
+# Draw circle
+# jorge.circle(50)
+for i in range(100):
+  jorge.circle(10+i, 45)
+#window.mainloop()
