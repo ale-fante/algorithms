1d4634390f03adabde3c44b3f0e4f0cedb2eac3b
ale-fante <alejandra.hledesma@gmail.com>
2020-02-12 18:43:53 -0800

    guessing game

--- tables.py
+++ tables.py
@@ -2,6 +2,16 @@ for x in range(13):
 	print(x, 2 ** x)
 
 
-for i in range(1, 7):
-	print(2 * i, "   ")
-print()
+# for i in range(1, 7):
+# 	print(2 * i, end="   ")
+# print()
+
+total = 0
+
+while True:
+	response = input("Enter the next number. (Leave blank to end) ")
+	if response == "" or response == "-1":
+		break
+	total += int(response)
+
+print("The total of the numbers you entered is ", total)
