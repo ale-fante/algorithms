9c84e5facabd7d9fd5c995129ff340a5002106ea
ale-fante <alejandra.hledesma@gmail.com>
2020-02-13 19:27:20 -0800

    Global vs Local variables

--- continue.py
+++ continue.py
@@ -1,6 +1,35 @@
 for i in [12, 16, 17, 24, 29, 30]:
+	# Pares
 	if i % 2:
 		continue
 	print(i)
 
 print("Done")
+
+celebs = [("Brad Pitt", 1963), ("Jack Nicholson", 1937), ("Betty White", 1920), ("Justin Bieber", 1994)]
+
+print(celebs)
+print(len(celebs))
+
+# print the names of those ceelebrities born before 1980:
+for name, year in celebs:
+	if name == "Brad Pitt":
+		print("Brad Pitt isn't as young as I thought...")
+	elif year < 1980:
+		print("Old peep: " + name, year)
+
+
+students = ["John", ["CompSci", "Physics"],
+			["Vusi", ["Maths", "Stats", "Music"],
+			["Jess", ["Econ", "English", "Art"],
+			["Sarah", ["Piano", "Law", "Italian", "Linguistics"],
+			["John", ["Sociology", "Stats", "Music"]
+]
+
+for name, subjects in students:
+oiutr   		  
+
+counter = 0
+for name, subjects in students:
+	if "CompSci" in subjects:
+		counter += 1
