bb7ac1b6f7ac808cf63a583e040f73abf7e6c889
ale-fante <alejandra.hledesma@gmail.com>
2020-02-22 19:13:32 -0800

    modifier vs pure functions

--- ch4/modifiers.py
+++ ch4/modifiers.py
@@ -10,3 +10,14 @@ def double_stuff(values):
 	return new_list
 
 print(double_stuff("Alejandra"))
+
+def el_doble(values):
+	""" Double th elements of values in-place """
+	for index, value in enumerate(values):
+		values[index] = 2 * value
+
+		print(values[index])
+
+el_doble([1, 2, 3])
+
+# Functions help us with our mental chunking: they allow us to group together statements for a high level purpose.
