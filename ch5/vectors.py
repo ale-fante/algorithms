667d6d5f4768dbd3d84c978352d2511e1889aa9a
ale-fante <alejandra.hledesma@gmail.com>
2020-03-08 11:21:27 -0700

    scalar vector

--- ch5/vectors.py
+++ ch5/vectors.py
@@ -9,3 +9,12 @@ one = [2, 4, 6]
 two = [1, 3, 5]
 
 print(add_vector(one, two))
+
+def scalar_mult(scalar, vector):
+	scalar_result = []
+	for i in range(len(vector)):
+		multi = scalar * vector[i]
+		scalar_result.append(multi)
+	return scalar_result
+
+print(scalar_mult(5, [1, 2]))
